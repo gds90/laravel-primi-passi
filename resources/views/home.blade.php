8f253d34c8b3d41a3f83b9e7c24f705fa86a730f
gds90 <you@example.com>
2024-02-09 14:28:58 +0100

    UPDATE: deleted welcome, added home.blade.php; Added an h1 with static Hello World

---
+++ resources/views/home.blade.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css">
+    <title>Laravel Hello World</title>
+</head>
+
+<body>
+    <main>
+        <div class="container">
+            <div class="row">
+                <div class="col-12 text-center">
+                    <h1>Hello World</h1>
+                </div>
+            </div>
+        </div>
+    </main>
+</body>
+
+</html>
